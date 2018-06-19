83279ad2e2087d032f803bd19f59872ef0d4f4f0
ema0312 <39930409+ema0312@users.noreply.github.com>
2018-06-18 19:31:35 -0500

    Add files via upload

---
+++ hw14.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw14
+{
+    class program
+    {
+        public void Homework() {
+            Console.WriteLine("this is homework four-teen!");
+            
+        }
+
+        private void Classwork() {
+            Console.WriteLine("our classwork is an app project");
+            
+        }
+
+        public int Lessons() {
+            int returnVariable = 1 + 12;
+            return returnVariable;
+        }
+
+        public void Grade(int _5, string _basic) {
+            Console.WriteLine("{0} is my midterm grade", _5);
+            Console.WriteLine("{0} is my effort", _basic);
+            Console.ReadKey();
+        }
+    }
+}
